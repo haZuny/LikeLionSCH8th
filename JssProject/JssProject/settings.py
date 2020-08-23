de8cf1c5414d7f124933402d6189135f79096365
haZuny <gkwns5791@likelion.org>
2020-08-23 15:32:51 +0900

    ㅎㅅㅎ

--- JssProject/JssProject/settings.py
+++ JssProject/JssProject/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewate',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'JssProject.urls'
